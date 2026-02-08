c1e5843939e75e7370bd874edf7c8ee1b9e4e559
SakhAlgo <sakhalog@gmail.com>
2026-02-08 20:52:05 +1100

    update tdk delete npm modules

--- tkd/script.js
+++ tkd/script.js
@@ -3,6 +3,28 @@ const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('navMenu');
 const navLinks = document.querySelectorAll('.nav-link');
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
 // Переключение мобильного меню
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
